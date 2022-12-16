ecbd3316528195d3d6d9c45b3bdfbd44fa2668ce
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-12-16 20:55:32 -0300

    Put everything together

---
+++ README.md
@@ -0,0 +1,18 @@
+# chatgpt irc bot
+
+I like irc and didn't find any chatgpt bot so I made this pretty simple and hacky one that doesn't do much besides just working.
+
+## Installation
+
+Clone this repo and run `npm install`. Requires `npm >= 18`.
+
+
+## Usage
+
+Copy `cp .env.example .env`, edit it with your own data. Start the bot with:
+
+```sh
+npm start
+```
+
+Mention the bot nickname in the irc channel it shows up and it will respond.
---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const IRC = require("irc-framework")
+
+async function main() {
+  const { ChatGPTAPI, getOpenAIAuth } = await import('chatgpt')
+  console.log("Creating bot...");
+  const openAIAuth = await getOpenAIAuth({
+    email: process.env.OPENAI_EMAIL,
+    password: process.env.OPENAI_PASSWORD
+  })
+  console.log("Bot created")
+
+  const api = new ChatGPTAPI({ ...openAIAuth })
+  await api.ensureAuth()
+
+
+  console.log("Connecting to IRC...")
+  var bot = new IRC.Client();
+  var channel_names = process.env.IRC_CHANNELS.split(",")
+  var buffers = [];
+
+  bot.connect({
+    host: process.env.IRC_HOST,
+    port: process.env.IRC_PORT,
+    nick: process.env.IRC_NICK,
+  });
+
+  bot.on('message', async function (event) {
+    if (event.message.indexOf('hello') === 0) {
+      event.reply('Hi!');
+    }
+    const nick_exp = "\\b" + process.env.IRC_NICK + "\\b[,:]?"
+    if (event.message.match(new RegExp(nick_exp))) {
+      const message = event.message.replace(new RegExp(nick_exp), "")
+      event.reply(await api.sendMessage(message))
+    }
+
+  });
+
+
+  bot.on('registered', function () {
+    console.log("Connected to IRC")
+    console.log("Joining channels...")
+    for (const channel_name of channel_names) {
+      var channel = bot.channel(channel_name);
+      channel.join();
+      channel.say('Hi! Ask me anything.');
+      buffers.push(channel);
+    }
+    console.log("Joined channels")
+  });
+
+}
+
+main();
